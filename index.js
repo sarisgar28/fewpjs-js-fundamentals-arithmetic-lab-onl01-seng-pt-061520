926daa862661f93e49bcde8722d18448959d5f54
sarisgar28 <sarisgar28@gmail.com>
2021-01-07 17:16:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 62;
+const num2 = 1;
+let multiply = num1 * num2;
+
+
+
+let random = Math.random();
+
+let mod =  24 % 5;
+
+let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+  
